f3991be978c9f5f1948bfd8be9f5d8a559e48bdc
christinch3n <c.jandrasits@gmail.com>
2014-10-25 17:57:13 +0200

    Changed name from Baltimore to Baltimore City.

--- plot2.R
+++ plot2.R
@@ -8,3 +8,5 @@ emission_sums <- rowsum(Baltimore$Emissions, as.factor(Baltimore$year))
 png("plot2.png", width=480, height=480)
 plot(rownames(emission_sums), emission_sums, main="Baltimore City - All Sources", xlab="Year", ylab="Total Emission from PM25", pch=8)
 dev.off()
+
+## Emissions in Baltimore City decreased, then increased and ended up much lower in 2008 compared to 1999.
