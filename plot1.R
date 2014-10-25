848a796f6bb5d550979c5433d67f3b271e0d2da8
christinch3n <c.jandrasits@gmail.com>
2014-10-25 15:16:49 +0200

    Added code and plots for first two questions.

---
+++ plot1.R
@@ -0,0 +1,11 @@
+setwd("C:/Users/Christine/Documents/git-repositories/courseradatascience/ExpData_project2")
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Sum up emissions per year (Pollutant is "PM25-PRI" in all rows!)
+emission_sums <- rowsum(NEI$Emissions, as.factor(NEI$year))
+
+png("plot1.png", width=480, height=480)
+plot(rownames(emission_sums), emission_sums, main="United States - All Sources", xlab="Year", ylab="Total Emission from PM25", pch=8)
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,12 @@
+setwd("C:/Users/Christine/Documents/git-repositories/courseradatascience/ExpData_project2")
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Sum up emissions per year (Pollutant is "PM25-PRI" in all rows!)
+Baltimore <- NEI[NEI$fips == "24510",]
+emission_sums <- rowsum(Baltimore$Emissions, as.factor(Baltimore$year))
+
+png("plot2.png", width=480, height=480)
+plot(rownames(emission_sums), emission_sums, main="Baltimore - All Sources", xlab="Year", ylab="Total Emission from PM25", pch=8)
+dev.off()
